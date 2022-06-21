ffa48dc5b3f424a7d47688d09fd8d6696d088b13
Miguel Tang <A01610836@itesm.mx>
2022-06-21 18:28:13 -0500

    create alert components

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -6,12 +6,20 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './warningAlert/warningAlert.component';
+import { SuccessAlert } from './successAlert/successAlert.component';
+import { Alerts } from './alerts/alerts.component';
+import { ErrorAlert } from './errorAlert/errorAlert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlert,
+    SuccessAlert,
+    ErrorAlert,
+    Alerts
   ],
   imports: [
     BrowserModule,
---
+++ my-first-app/src/app/errorAlert/errorAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'error-alert',
+    templateUrl: './errorAlert.component.html',
+    styleUrls: ['./errorAlert.component.css']
+})
+export class ErrorAlert {
+
+}
