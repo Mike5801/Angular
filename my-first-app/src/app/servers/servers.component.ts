986c4543d899ee4d85d2915912f00f549abf8f6c
Miguel Tang <A01610836@itesm.mx>
2022-06-22 17:03:34 -0500

    add directive ngIf with else

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -10,6 +10,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName: string = 'TestServer';
+  serverCreated = false;
+
   constructor() { 
     setTimeout( () => {
       this.allowNewServer = true;
@@ -21,8 +23,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
-
   }
 
   onUpdateServerName(event: any) {
