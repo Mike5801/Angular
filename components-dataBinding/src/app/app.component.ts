c38b34a1bb65a164f66331a636aa431871232394
Miguel Tang <A01610836@itesm.mx>
2022-06-24 12:58:43 -0500

    add custom events in cockpit component and bind them with app component

--- components-dataBinding/src/app/app.component.ts
+++ components-dataBinding/src/app/app.component.ts
@@ -9,4 +9,21 @@ export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content:'Just a test!'}];
   
   title = 'components-dataBinding';
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+
 }
