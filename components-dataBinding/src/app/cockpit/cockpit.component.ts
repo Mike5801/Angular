3d8bc33be9993a2d0066b47ccf6f0792654265db
Miguel Tang <A01610836@itesm.mx>
2022-06-27 15:00:22 -0500

    change dual binding for ViewChild on server content

--- components-dataBinding/src/app/cockpit/cockpit.component.ts
+++ components-dataBinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -15,7 +15,8 @@ export class CockpitComponent implements OnInit {
     blueprintContent: string
   }>();
   //newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true }) serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -25,14 +26,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value, 
-      serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       blueprintName: nameInput.value,
-      blueprintContent: this.newServerContent
+      blueprintContent: this.serverContentInput.nativeElement.value
     })
   }
 
