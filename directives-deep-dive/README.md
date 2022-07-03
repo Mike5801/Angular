ba4088f6ba7e39aa916fd8f4802080723d2d6a83
Miguel Tang <A01610836@itesm.mx>
2022-07-03 13:30:35 -0500

    delete course-project folder and add bootstrap

--- directives-deep-dive/README.md
+++ directives-deep-dive/README.md
@@ -1,4 +1,4 @@
-# CourseProject
+# DirectivesDeepDive
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.2.
 
--- directives-deep-dive/src/app/app.component.spec.ts
+++ directives-deep-dive/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'course-project'`, () => {
+  it(`should have as title 'directives-deep-dive'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('course-project');
+    expect(app.title).toEqual('directives-deep-dive');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('course-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('directives-deep-dive app is running!');
   });
 });
