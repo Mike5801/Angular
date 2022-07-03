150b439c397b9a72318c12d881c89d82a1b969c5
Miguel Tang <A01610836@itesm.mx>
2022-07-03 14:21:13 -0500

    add host listener to style components

--- directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
+++ directives-deep-dive/src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,9 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
-
   constructor(
     private elRef: ElementRef, 
     private renderer: Renderer2) 
@@ -16,8 +15,18 @@ export class BetterHighlightDirective implements OnInit{
       //  attribute you want to style
       //  value of the attribute
       //  flag(optional such as #important)
-      this.renderer.setStyle(
-        this.elRef.nativeElement, 'background-color', 'blue');
+      //this.renderer.setStyle(
+      //  this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement, 'background-color', 'transparent');
   }
 }
 
