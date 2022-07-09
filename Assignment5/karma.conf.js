f917ae83cf24e6d2101bea8557eb9dead4eb0202
Miguel Tang <A01610836@itesm.mx>
2022-07-09 15:57:15 -0500

    remove services folder

--- Assignment5/karma.conf.js
+++ Assignment5/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/services-dependencies'),
+      dir: require('path').join(__dirname, './coverage/assignment5'),
       subdir: '.',
       reporters: [
         { type: 'html' },
