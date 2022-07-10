709f0612df826a5628f604800a4682313443bc22
Miguel Tang <A01610836@itesm.mx>
2022-07-10 14:50:16 -0500

    change implementation to service

--- Assignment5/src/app/inactive-users/inactive-users.component.ts
+++ Assignment5/src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,18 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
+
 export class InactiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+
+  constructor(private usersService: UsersService) { }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.usersService.onSetToActive(id);
   }
 }
